3493cfd2cc45bb3a54379dca8f77f02768612d93
luaenrique <lua.enriquezan@gmail.com>
2024-12-03 15:43:29 -0300

    feat: generic encoder model

---
+++ main.py
@@ -0,0 +1,171 @@
+from transformers import ElectraForSequenceClassification
+from transformers import AutoTokenizer
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import AdamW
+
+from transformers import ElectraTokenizer
+
+from transformers import LongformerForSequenceClassification
+from transformers import LongformerTokenizer
+
+from transformers import BertForSequenceClassification
+from transformers import BertTokenizer
+
+from transformers import RobertaForSequenceClassification
+from transformers import RobertaTokenizer
+from transformers import AutoModelForSequenceClassification
+
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
+from transformers import EvalPrediction
+import torch
+import numpy as np
+import evaluate
+
+from transformers import TrainingArguments, Trainer
+batch_size = 8
+metric_name = "accuracy"
+
+
+
+class GenericEncoderModel:
+    def __init__(self, model_name, training_file_name, model_type, problem_type):
+        self.data = []
+        self.model_name = model_name
+        self.training_file_name = training_file_name
+        self.model_type = model_type
+        self.problem_type = problem_type
+        self.tokenizer = self._load_tokenizer()
+        self.model = self._load_model()
+        self.trainer = None
+
+    def _load_tokenizer(self):
+        if self.model_type == 'electra':
+            tokenizer = ElectraTokenizer.from_pretrained(self.model_name)
+        elif self.model_type == 'longformer':
+            tokenizer = LongformerTokenizer.from_pretrained(self.model_name)
+        elif self.model_type == 'bert':
+            tokenizer = BertTokenizer.from_pretrained(self.model_name)
+        elif self.model_type == 'roberta':
+            tokenizer = RobertaTokenizer.from_pretrained(self.model_name)
+        else:
+            raise ValueError(f"Unsupported model type: {self.model_type}")
+        
+        return tokenizer
+    
+    def _load_model(self):
+        model = AutoModelForSequenceClassification.from_pretrained(self.model_name,
+                                                           problem_type=self.problem_type)
+        return model
+
+    def compute_metrics(self, eval_preds, threshold = 0.5):
+        logits, labels = eval_preds
+        if self.problem_type == "single_label_classification" :
+            # single label classification
+            ptype = None
+            predictions = np.argmax(logits, axis=-1).reshape(-1,1)
+            labels_ = labels
+            metrics = ["accuracy", "micro-f1", "macro-f1"]
+        elif self.problem_type ==  "multi_label_classification":
+            # multi label classification
+            ptype = "multilabel"
+            sigmoid = torch.nn.Sigmoid()
+            probs = sigmoid(torch.Tensor(logits))
+            predictions = np.zeros(probs.shape)
+            predictions[np.where(probs > threshold)] = 1
+            predictions = predictions.astype('int32')
+            labels_ = labels.astype('int32')
+            # labels_ = labels
+            metrics = ["micro-f1", "macro-f1"]
+        else:
+            raise ValueError("Wrong problem type")
+        # Compute the output
+        outputs = dict()
+        if "accuracy" in metrics:
+            metric = evaluate.load("accuracy")
+            accuracy = metric.compute(predictions=predictions, references=labels_)
+            outputs["accuracy"] = accuracy["accuracy"]
+        if "micro-f1" in metrics:
+            metric = evaluate.load("f1", ptype)
+            f1_micro = metric.compute(predictions=predictions, references=labels_, average = 'micro')
+            outputs["micro-f1"] = f1_micro["f1"]
+        if "macro-f1" in metrics:
+            metric = evaluate.load("f1",  ptype)
+            f1_macro = metric.compute(predictions=predictions, references=labels_, average = 'macro')
+            outputs["macro-f1"] = f1_macro["f1"]
+        return outputs
+    
+    def train(self, train_dataset, test_dataset):
+        args = TrainingArguments(
+            self.training_file_name,
+            evaluation_strategy = "epoch",
+            save_strategy = "epoch",
+            learning_rate=2e-5,
+            per_device_train_batch_size=batch_size,
+            per_device_eval_batch_size=batch_size,
+            num_train_epochs=5,
+            weight_decay=0.01,
+            load_best_model_at_end=True,
+            metric_for_best_model=metric_name,
+            #push_to_hub=True,
+        )
+        trainer = Trainer(
+            self.model,
+            args,
+            train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            tokenizer=self.tokenizer,
+            compute_metrics=self.compute_metrics
+        )
+        trainer.train()
+        self.trainer = trainer
+
+    def evaluate(self):
+        return self.trainer.evaluate()
+    
+
+
+from datasets import load_dataset
+
+
+# Importing the Amazon dataset recommended by Cristiano
+amazon_dataset = load_dataset("fancyzhx/amazon_polarity")
+
+
+datasets = [amazon_dataset]
+
+bertModel = GenericEncoderModel(
+    model_name='bert-base-uncased', 
+    training_file_name='bert_training', 
+    model_type='bert', 
+    problem_type='single_label_classification'
+)
+
+models = [bertModel]
+
+
+def preprocess_function(examples, tokenizer):
+    return tokenizer(examples["content"], truncation=True, padding="max_length", max_length=128)
+
+for dataset in datasets:
+    contentList = dataset['train'].take(6000)['content']
+    labelList = dataset['train'].take(6000)['label']
+
+    contentTestList = dataset['test'].take(1000)['content']
+    labelTestList = dataset['test'].take(1000)['label']
+
+    for model in models:
+        train_dataset = dataset['train'].take(6000).map(lambda x: preprocess_function(x, model.tokenizer), batched=True)
+        test_dataset = dataset['test'].take(1000).map(lambda x: preprocess_function(x, model.tokenizer), batched=True)
+        train_dataset = train_dataset.map(remove_columns=['content', 'title'])
+
+        example = train_dataset[0]
+        print(example.keys())
+
+        print(model.tokenizer.decode(example['input_ids']))
+
+        train_dataset.set_format("torch")
+
+        model.train(train_dataset=train_dataset, test_dataset=test_dataset)
+
+        print(model.evaluate())
