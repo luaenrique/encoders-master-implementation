6fbdc9d8940f14695a1077ca3133cd7a6571ccea
luaenrique <lua.enriquezan@gmail.com>
2025-09-01 20:09:11 -0300

    fix: test

--- imdb.py
+++ imdb.py
@@ -110,7 +110,7 @@ class GenericEncoderModel:
 
         args = TrainingArguments(
             f"{self.training_file_name}_{dataset_name}_2",
-            evaluation_strategy = "epoch",
+            eval_strategy = "epoch",
             save_strategy = "epoch",
             learning_rate=2e-5,
             per_device_train_batch_size=batch_size,
